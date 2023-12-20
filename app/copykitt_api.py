580aafe3f980581dca40206fb854f530eb1a3552
Nico <126928196+anibarro022@users.noreply.github.com>
2023-12-19 16:38:27 -0800

    CREATE API Endpoints

---
+++ app/copykitt_api.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, HTTPException
+from copykitt import generate_branding_snippet, generate_keywords
+
+app = FastAPI()
+
+MAX_INPUT_LENGTH = 24
+
+
+@app.get("/generate_snippet")
+async def generate_snippet_api(prompt: str):
+    validate_input_length(prompt)
+    snippet = generate_branding_snippet(prompt)
+    return {"snippet": snippet, "keywords": []}
+
+@app.get("/generate_keywords")
+async def generate_keywords_api(prompt: str):
+    validate_input_length(prompt)
+    keywords = generate_keywords(prompt)
+    return {"snippet": None, "keywords": keywords}
+
+@app.get("/generate_snippit_and_keywords")
+async def generate_snippit_and_keywords_api(prompt: str):
+    validate_input_length(prompt)
+    snippet = generate_branding_snippet(prompt)
+    keywords = generate_keywords(prompt)
+    return {"snippet": snippet, "keywords": keywords}
+
+def validate_input_length(prompt: str):
+    if len(prompt) >= MAX_INPUT_LENGTH:
+        raise HTTPException(status_code=400, detail=f"Input length must be under {MAX_INPUT_LENGTH} characters")
+    pass
